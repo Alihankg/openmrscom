16b94b2f2b352ca4bc03ac21c27a2bf14370e7a3
alikhan <kebrun2017@gmail.com>
2023-09-24 19:54:27 +0300

    initial commit 2

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,2 +1,82 @@
-package Utility;public class BaseDriver {
+package Utility;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+
+    public static final org.apache.logging.log4j.Logger logger4j= LogManager.getLogger();
+
+    public static WebDriver driver; // SingletonDriver method
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void baslangicIslemleri(){
+        Logger logger= Logger.getLogger(""); // output yapılan logları al.
+        logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
+
+        driver = new ChromeDriver(); // jenkins deyken : sen head olmadan yani hafızada çalış
+        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        loginTesti();
+    }
+
+    public void loginTesti(){
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+        MyFunc.Bekle(2);
+
+        WebElement inputEmail = driver.findElement(By.id("input-email"));
+        inputEmail.sendKeys("testng1@gmail.com");
+
+        WebElement inputpassword = driver.findElement(By.id("input-password"));
+        inputpassword.sendKeys("123qweasd");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        loginBtn.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"));
+    }
+
+    @AfterClass
+    public void bitisIslemleri(){ // tearDown
+        MyFunc.Bekle(5);
+        driver.quit();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        //System.out.println("test metodu başladı");
+        logger4j.info("test metodu başladı");
+        logger4j.warn("warning mesaj test başladı");
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult sonuc){ // tesin sonuç ve isim bilgisini olduğu değişkeni vereyim mi
+        //System.out.println("test metodu bitti");
+        logger4j.info(sonuc.getName() +" test metodu bitti " + (sonuc.getStatus()==1 ? " passed " : "fail") );
+        logger4j.warn("warning mesaj test bitti");
+
+        // excel yazabileceksin
+        // dosyaya da yazabileceksin
+    }
+
+
 }
--- src/Utility/MyFunc.java
+++ src/Utility/MyFunc.java
@@ -1,2 +1,44 @@
-package Utility;public class MyFunc {
+package Utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class MyFunc {
+
+    public static void Bekle(int sn){
+
+        try {
+            Thread.sleep(sn*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    // Çoğu yerde aynı mesaj doğrulaması kullanıldığı için bu bölümü
+    // ortak fonksiyonların yer aldığı MyFunc a taşındı.
+    public static void successMessageValidation(){
+        WebElement msgLabel=BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+    }
+
+    public static int randomGenerator(int sinir){
+        return (int)(Math.random()*sinir);
+    }
+
+    public static boolean listContainsString(List<WebElement> list, String aranacakKelime){
+        boolean bulundu=false;
+        for(WebElement e: list){
+            if (e.getText().toLowerCase().equals(aranacakKelime.toLowerCase()))
+            {
+                bulundu=true;
+                break;
+            }
+        }
+
+        return bulundu;
+    }
 }
